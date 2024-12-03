8491e004fa3333add19ee886ebd7195f795c80a7
tuanld3110_mm <99404917+tuan31102001@users.noreply.github.com>
2024-12-03 17:02:45 +0700

    Feature/login screen (#13)
    * TuanLD11: update footer
    
    * TuanLD11: update footer
    
    * TuanLD11: update UI login_screen

--- lib/commons/widgets/footer.dart
+++ lib/commons/widgets/footer.dart
@@ -7,53 +7,112 @@ class Footer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFFFFFFFF),
-      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 195),
-      child: Column(
-        children: [
-          const Divider(
-            color: Color(0xFFFAFAFA),
-            thickness: 1,
-          ),
-          const SizedBox(height: 20),
-          const Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 195),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              FooterColumn(
-                title: "Company Info",
-                items: ["About Us", "Carrier", "We are hiring", "Blog"],
+              const Text(
+                "Bandage",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
+                  color: Color(0xFF252B42),
+                ),
               ),
-              FooterColumn(
-                title: "Legal",
-                items: [
-                  "Privacy Policy",
-                  "Terms of Service",
-                  "Disclaimer",
-                  "Cookie Policy"
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Image.asset(
+                      "assets/icons/facebook.png",
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Image.asset(
+                      "assets/icons/instagram.png",
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Image.asset(
+                      "assets/icons/twitter.png",
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
                 ],
               ),
-              FooterColumn(
-                title: "Features",
-                items: [
-                  "Business Marketing",
-                  "User Analytics",
-                  "Live Chat",
-                  "Unlimited Support"
-                ],
-              ),
-              FooterColumn(
-                title: "Resources",
-                items: ["iOS & Android", "Watch a Demo", "Customers", "API"],
-              ),
-              FooterSubscribe(),
             ],
           ),
-          const SizedBox(height: 30),
-          Divider(color: Colors.grey[400], thickness: 1),
-          const SizedBox(height: 20),
-          const Row(
+        ),
+        Container(
+          color: const Color(0xFFFFFFFF),
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 195),
+          child: const Column(
+            children: [
+              Divider(
+                color: Color(0xFFE6E6E6),
+                thickness: 1,
+              ),
+              SizedBox(height: 50),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FooterColumn(
+                    title: "Company Info",
+                    items: ["About Us", "Carrier", "We are hiring", "Blog"],
+                  ),
+                  FooterColumn(
+                    title: "Legal",
+                    items: [
+                      "Privacy Policy",
+                      "Terms of Service",
+                      "Disclaimer",
+                      "Cookie Policy"
+                    ],
+                  ),
+                  FooterColumn(
+                    title: "Features",
+                    items: [
+                      "Business Marketing",
+                      "User Analytics",
+                      "Live Chat",
+                      "Unlimited Support"
+                    ],
+                  ),
+                  FooterColumn(
+                    title: "Resources",
+                    items: [
+                      "iOS & Android",
+                      "Watch a Demo",
+                      "Customers",
+                      "API"
+                    ],
+                  ),
+                  FooterSubscribe(),
+                ],
+              ),
+              SizedBox(height: 52),
+            ],
+          ),
+        ),
+        Container(
+          color: const Color(0xFFFAFAFA),
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 195),
+          child: const Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Text(
@@ -66,8 +125,8 @@ class Footer extends StatelessWidget {
               ),
             ],
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
