400398506701bd09df3b4c06ac575d35702566a8
hoang <hoangluckyexoo@gmail.com>
2024-12-03 15:08:36 +0700

    repair coding convention

--- lib/commons/constants/app_svg.dart
+++ lib/commons/constants/app_svg.dart
@@ -1,12 +1,12 @@
 class AppSvgs {
-  static const String icBrand1 = 'assets/icons/ic_Brand1.svg';
-  static const String icBrand2 = 'assets/icons/ic_Brand2.svg';
-  static const String icBrand3 = 'assets/icons/ic_Brand3.svg';
-  static const String icBrand4 = 'assets/icons/ic_Brand4.svg';
-  static const String icBrand5 = 'assets/icons/ic_Brand5.svg';
-  static const String icBrand6 = 'assets/icons/ic_Brand6.svg';
-  static const String iconList = 'assets/icons/icon_List.svg';
-  static const String iconList2 = 'assets/icons/icon_List2.svg';
-  static const String icRowDown = 'assets/icons/ic_RowDown.svg';
-  static const String icRowRight = 'assets/icons/ic_RowRight.svg';
+  static const String icBrand1 = 'assets/icons/ic_brand1.svg';
+  static const String icBrand2 = 'assets/icons/ic_brand2.svg';
+  static const String icBrand3 = 'assets/icons/ic_brand3.svg';
+  static const String icBrand4 = 'assets/icons/ic_brand4.svg';
+  static const String icBrand5 = 'assets/icons/ic_brand5.svg';
+  static const String icBrand6 = 'assets/icons/ic_brand6.svg';
+  static const String iconList = 'assets/icons/icon_list.svg';
+  static const String iconList2 = 'assets/icons/icon_list2.svg';
+  static const String icRowDown = 'assets/icons/ic_rowDown.svg';
+  static const String icRowRight = 'assets/icons/ic_rowRight.svg';
 }
