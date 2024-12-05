e0436856adb0b5841d4e24f96773f3041807439b
DESKTOP-U8I23PI\Admin <daothanhdat2801@gmail.com>
2024-12-05 19:15:35 +0700

    Fix background_card cause crashing app

--- lib/pages/home/widgets/background_card.dart
+++ lib/pages/home/widgets/background_card.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_web_app/commons/constants/app_color.dart';
 import 'package:ecommerce_web_app/commons/constants/app_img.dart';
+import 'package:ecommerce_web_app/commons/utils/extensions.dart';
 import 'package:flutter/material.dart';
 
 class BackgroundCard extends StatelessWidget {
@@ -9,32 +10,39 @@ class BackgroundCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Expanded(
-        child: Image.asset(
+    final width = context.deviceSize.width;
+    return SizedBox(
+      height: width * 0.4,
+      width: width,
+      child: Stack(children: [
+        Image.asset(
           AppImage.clothHome,
+          width: width,
+          fit: BoxFit.fitWidth,
         ),
-      ),
-      Positioned(
-        bottom: 40,
-        left: 250,
-        child: ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
+        Positioned(
+          bottom: 40,
+          left: 250,
+          child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
               ),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-              child: Text(
-                "Shop now",
-                style: TextStyle(
-                    fontSize: 14, fontWeight: FontWeight.w700, color: AppColors.white),
-              ),
-            )),
-      )
-    ]);
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                child: Text(
+                  "Shop now",
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      color: AppColors.white),
+                ),
+              )),
+        )
+      ]),
+    );
   }
 }
